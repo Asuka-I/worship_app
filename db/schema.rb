7c4474634698a60f8f0d0492983683d648e20aa6
Asuka-I <1400cm@gmail.com>
2021-08-17 23:44:03 +0900

    commentモデル commentsテーブル を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_141516) do
+ActiveRecord::Schema.define(version: 2021_08_17_144120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "seals", force: :cascade do |t|
     t.string "category", null: false
