d4e758eaa95ccdf737a2c9df66264b61ddabf826
Asuka-I <1400cm@gmail.com>
2021-08-18 00:45:06 +0900

    rubocop -a を実行

--- db/migrate/20210817135409_add_columns_to_worships.rb
+++ db/migrate/20210817135409_add_columns_to_worships.rb
@@ -1,9 +1,11 @@
 class AddColumnsToWorships < ActiveRecord::Migration[6.1]
   def change
-    add_column :worships, :content, :text
-    add_column :worships, :date, :date
-    add_column :worships, :image, :string
-    add_column :worships, :rating, :float
-    add_column :worships, :likes_count, :integer
+    hange_table :worships, bulk: true do |t|
+      t.text :content
+      t.date :date
+      t.string :image
+      t.float :rating
+      t.integer :likes_count
+    end
   end
 end
--- db/migrate/20210817141516_create_seals.rb
+++ db/migrate/20210817141516_create_seals.rb
@@ -1,10 +1,10 @@
 class CreateSeals < ActiveRecord::Migration[6.1]
   def change
     create_table :seals do |t|
-      t.string :category, null:false
+      t.string :category, null: false
       t.string :title, presence: true, length: { maximum: 50 }
       t.string :place, presence: true, length: { maximum: 50 }
-      t.date :date, null:false
+      t.date :date, null: false
       t.string :image
       t.integer :likes_count, default: 0
 
