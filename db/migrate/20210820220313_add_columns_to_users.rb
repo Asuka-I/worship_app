cdc670e76b2440920de15f0d9a294c0c8625b8d7
Asuka-I <1400cm@gmail.com>
2021-08-21 07:43:39 +0900

    usersテーブル バリデーション変更

--- db/migrate/20210820220313_add_columns_to_users.rb
+++ db/migrate/20210820220313_add_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
     change_table :users, bulk: true do |t|
-      t.string :name, null: false
+      t.string :name
       t.string :avatar
       t.text :introduce
     end
