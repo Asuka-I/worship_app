b6fe51a9fe4574ff72f460330cdfba24fcc9fb40
Asuka-I <1400cm@gmail.com>
2021-09-12 19:06:04 +0900

    ビューに コメント欄を作成、投稿したコメントの表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    current_user.comments.create!(comment_params)
+    current_user.comments.create(comment_params)
+    redirect_to root_path
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, worship_id: params[:worship_id])
   end
 end
