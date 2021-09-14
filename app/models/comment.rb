6ddd7ec11daa3e24475c15917ad0743e01cf5263
Asuka-I <1400cm@gmail.com>
2021-09-14 18:57:53 +0900

    rubocop -a,rails best practice を 実行 指摘箇所を修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :worship
   belongs_to :user
+
+  delegate :name, to: :user, prefix: true
 end
--- app/models/worship.rb
+++ app/models/worship.rb
@@ -3,7 +3,7 @@ class Worship < ApplicationRecord
 
   belongs_to :user
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   delegate :name, to: :user, prefix: true, allow_nil: true
 end
