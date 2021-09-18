3b3729946e63dbc1953382e54515d4608613fc1e
Asuka-I <1400cm@gmail.com>
2021-09-19 00:48:35 +0900

    御朱印帳の新規投稿機能を実装 不要なviewファイルを削除

--- app/controllers/seals_controller.rb
+++ app/controllers/seals_controller.rb
@@ -1,17 +1,29 @@
 class SealsController < ApplicationController
   def index
-    @seals = Worship.all.includes(:user).order(:created_at)
+    @seals = Seal.all.includes(:user).order(:created_at)
   end
 
   def show; end
 
-  def new; end
+  def new
+    @seal = Seal.new
+  end
 
-  def create; end
+  def create
+    seal = current_user.seals.create!(seal_params)
+    redirect_to seal
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def seal_params
+    params.require(:seal).permit(:category, :title, :place, :date)
+  end
+
 end
