9860d4dadd16f0c00da325cc467e3f8a35bb96ea
Asuka-I <1400cm@gmail.com>
2021-09-19 01:28:39 +0900

    御朱印帳の削除機能を実装

--- app/controllers/seals_controller.rb
+++ app/controllers/seals_controller.rb
@@ -1,4 +1,7 @@
 class SealsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_seal, only: %i[edit update destroy]
+
   def index
     @seals = Seal.all.includes(:user).order(:created_at)
   end
@@ -20,12 +23,19 @@ class SealsController < ApplicationController
 
   def update; end
 
-  def destroy; end
+  def destroy
+    @seal.destroy!
+    redirect_to @seal
+  end
 
   private
 
   def seal_params
     params.require(:seal).permit(:category, :title, :place, :date)
+  end
+
+  def set_seal
+    @seal = current_user.seals.find(params[:id])
   end
 
 end
