7580fffc566a7216ffc0b3b42bde71e9d79cc96f
Asuka-I <1400cm@gmail.com>
2021-09-20 22:55:20 +0900

    sealsコントローラの seal_params にパラメータを追加

--- db/migrate/20210920092648_change_datatype_images_of_seals.rb
+++ db/migrate/20210920092648_change_datatype_images_of_seals.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeImagesOfSeals < ActiveRecord::Migration[6.1]
   def change
-    change_column :seals, :images, 'json USING CAST(images AS json)'
+    change_column :seals, :images, "json USING CAST(images AS json)"
   end
 end
