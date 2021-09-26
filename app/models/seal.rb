f7f39a0b2f0e5790e5c1921ded025b18ba043e20
Asuka-I <1400cm@gmail.com>
2021-09-26 17:55:36 +0900

    特定のユーザー、投稿が「いいね！」したユーザーと投稿の一覧を取得できるように各モデルに追記

--- app/models/seal.rb
+++ app/models/seal.rb
@@ -1,6 +1,7 @@
 class Seal < ApplicationRecord
   belongs_to :user
   has_many :seal_likes, dependent: :destroy
+  has_many :seal_liked_users, through: :seal_likes, source: :user
 
   mount_uploaders :images, SealImageUploader
 
--- app/models/worship.rb
+++ app/models/worship.rb
@@ -2,8 +2,10 @@ class Worship < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :worship_likes, dependent: :destroy
+  has_many :worship_liked_users, through: :worship_likes, source: :user
 
   mount_uploaders :images, ImageUploader
 
   delegate :name, to: :user, prefix: true, allow_nil: true
+
 end
