7f463f271987701503012aa164aef8dd3dad8852
Asuka-I <1400cm@gmail.com>
2021-09-26 19:31:29 +0900

    user.rb に いいね のリレーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :worships, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :seals, dependent: :destroy
+  has_many :worship_likes, dependent: :destroy
+  has_many :seal_likes, dependent: :destroy
   has_many :liked_worships, through: :worship_likes, source: :worship
   has_many :liked_seals, through: :seal_likes, source: :seal
   # Include default devise modules. Others available are:
