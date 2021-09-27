be9af66f5e16e1dd54768e8fd026f7e5b71c454a
Asuka-I <1400cm@gmail.com>
2021-09-27 21:52:06 +0900

    各コントローラを修正

--- app/controllers/seal_likes_controller.rb
+++ app/controllers/seal_likes_controller.rb
@@ -1,11 +1,11 @@
 class SealLikesController < ApplicationController
   def create
-    current_user.seal_likes.create!(seal_id: params[:seal_id])
-    redirect_back(fallback_location: root_path)
+    @seal = Seal.find(params[:seal_id])
+    current_user.seal_likes.create!(seal_id: @seal.id)
   end
 
   def destroy
-    current_user.seal_likes.find_by(seal_id: params[:seal_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @seal = Seal.find(params[:seal_id])
+    current_user.seal_likes.find_by(seal_id: @seal.id).destroy!
   end
 end
--- app/controllers/worship_likes_controller.rb
+++ app/controllers/worship_likes_controller.rb
@@ -1,11 +1,11 @@
 class WorshipLikesController < ApplicationController
   def create
-    current_user.worship_likes.create!(worship_id: params[:worship_id])
-    redirect_back(fallback_location: root_path)
+    @worship = Worship.find(params[:worship_id])
+    current_user.worship_likes.create!(worship_id: @worship.id)
   end
 
   def destroy
-    current_user.worship_likes.find_by(worship_id: params[:worship_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @worship = Worship.find(params[:worship_id])
+    current_user.worship_likes.find_by(worship_id: @worship.id).destroy!
   end
 end
