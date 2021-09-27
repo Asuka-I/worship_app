050e10d33de4cad5168d2a4087a8a4770fadbf88
Asuka-I <1400cm@gmail.com>
2021-09-27 22:15:05 +0900

    各モデルにcounter_cache を設定

--- app/models/seal_like.rb
+++ app/models/seal_like.rb
@@ -1,6 +1,6 @@
 class SealLike < ApplicationRecord
   belongs_to :user
-  belongs_to :seal
+  belongs_to :seal, counter_cache: :likes_count
   validates :user_id, uniqueness: {
     scope: :seal_id,
     message: "は同じ投稿に2回以上いいねはできません"
--- app/models/worship_like.rb
+++ app/models/worship_like.rb
@@ -1,6 +1,6 @@
 class WorshipLike < ApplicationRecord
   belongs_to :user
-  belongs_to :worship
+  belongs_to :worship, counter_cache: :likes_count
   validates :user_id, uniqueness: {
     scope: :worship_id,
     message: "は同じ投稿に2回以上いいねはできません"
