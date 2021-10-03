542728f3afde76783c0cd98c2c862e021a6af0ee
Asuka-I <1400cm@gmail.com>
2021-10-03 18:51:14 +0900

    Userモデルにメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,19 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
+
+  # フォローする
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォロー解除
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしてるかどうか
+  def following?(user)
+    followings.include?(user)
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
