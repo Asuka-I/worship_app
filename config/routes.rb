b0a8c72535ccda0002e83ca8a68c993e4fa0d135
Asuka-I <1400cm@gmail.com>
2021-10-03 19:04:20 +0900

    ルーティングに記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   root "homes#index"
+
   devise_for :users
-  resource :users, only: :show
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
+  end
+
   resources :seals do
     resource :seal_likes, only: [:create, :destroy]
   end
+  
   resources :worships do
     resources :comments, only: :create
     resource :worship_likes, only: [:create, :destroy]
