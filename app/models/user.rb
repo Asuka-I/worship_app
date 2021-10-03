57e76542037b60c154c65b07da6411360fd18874
Asuka-I <1400cm@gmail.com>
2021-10-03 18:47:52 +0900

    アソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   has_many :worships, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :seals, dependent: :destroy
+  # いいね関連
   has_many :worship_likes, dependent: :destroy
   has_many :seal_likes, dependent: :destroy
   has_many :liked_worships, through: :worship_likes, source: :worship
   has_many :liked_seals, through: :seal_likes, source: :seal
+  # フォロー関連
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
