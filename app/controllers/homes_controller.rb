e83e050c73f177fee52d03362461f753c3cb96a6
Asuka-I <1400cm@gmail.com>
2021-10-17 21:18:58 +0900

    トップページの記事一覧を新着順に変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @worships = Worship.all.includes(:user).order(:created_at).limit(8)
-    @seals = Seal.all.includes(:user, :seal_likes).order(:created_at).limit(8)
+    @worships = Worship.all.includes(:user).order(created_at: :desc).limit(8)
+    @seals = Seal.all.includes(:user, :seal_likes).order(created_at: :desc).limit(8)
   end
 end
