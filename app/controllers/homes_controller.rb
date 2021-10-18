3da3600597cdbf2e7fa8b69eda315b90f1d5f476
Asuka-I <1400cm@gmail.com>
2021-10-18 22:36:52 +0900

    rubocop -a を実行

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   def index
     @worships = Worship.all.includes(:user).order(created_at: :desc).limit(8)
     @seals = Seal.all.includes(:user, :seal_likes).order(created_at: :desc).limit(8)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
-  
+
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
