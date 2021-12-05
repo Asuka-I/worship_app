2a3139007712dbb4a3e9c0d09c662cb47f1a6010
Asuka-I <1400cm@gmail.com>
2021-12-05 22:45:59 +0900

    rubocop -a を実行

--- app/javascript/packs/slick.js
+++ app/javascript/packs/slick.js
@@ -1,13 +1,11 @@
-document.addEventListener("turbolinks:load"
-, function () {
-
+document.addEventListener('turbolinks:load', function () {
   $('.thumbnail').slick({
     autoplay: true, //自動再生
     arrows: false, //矢印非表示
     fade: true, //フェードの有効化
     speed: 1000, //切り替えのスピード（小さくすると速くなる）
     draggable: false, //ドラッグ操作の無効化
-    asNavFor:'.thumbnail-thumb',
+    asNavFor: '.thumbnail-thumb',
   });
   $('.thumbnail-thumb').slick({
     slidesToShow: 4, //表示するスライドの数
@@ -15,5 +13,4 @@ document.addEventListener("turbolinks:load"
     asNavFor: '.thumbnail', //thumbnailクラスのナビゲーション
     arrows: false, //矢印を非表示
   });
-
 });
