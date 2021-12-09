958a43091cf72f8dbbb8a059db168a285587c37a
Asuka-I <1400cm@gmail.com>
2021-12-09 23:51:16 +0900

    application_controller.rb と ヘッダーの検索フォームを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_q
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def search
+    @results = @q.result
+  end
+
+  private
+  
+  def set_q
+    @q = Worship.ransack(params[:q])
   end
 end
