8c9b56a2563f5ce92a74075de606e26289887db5
Asuka-I <1400cm@gmail.com>
2022-01-10 02:03:21 +0900

    各モデルのデータ更新ができるように修正

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,18 +1,4 @@
 ActiveAdmin.register Comment do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :content, :user_id, :worship_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:content, :user_id, :worship_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+permit_params :content
 end
--- app/admin/seals.rb
+++ app/admin/seals.rb
@@ -1,18 +1,4 @@
 ActiveAdmin.register Seal do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :category, :prefecture_id, :place, :date, :images, :likes_count, :user_id, :address
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:category, :prefecture_id, :place, :date, :images, :likes_count, :user_id, :address]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+permit_params :category, :prefecture_id, :place, :date, :images, :address, :likes_count
 end
