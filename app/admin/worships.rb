ca14a27a243ce3445859dfebbf53a7a564359d34
Asuka-I <1400cm@gmail.com>
2022-01-10 23:29:48 +0900

    日本語化を修正

--- app/admin/worships.rb
+++ app/admin/worships.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Worship do
-  permit_params :category, :prefecture_id, :place, :content, :date, :images, :address, :likes_count, :user_id  
+  permit_params :category, :prefecture_id, :place, :content, :date, :images, :address, :likes_count, :user_id
 end
