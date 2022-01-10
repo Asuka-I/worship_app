16b2c7e15bf1db6a4f254e0bb17ce620eb84d137
Asuka-I <1400cm@gmail.com>
2022-01-11 01:09:51 +0900

    ActiveAdmin のユーザーの検索条件を編集

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register User do
   permit_params :name, :email, :avatar, :introduce, :password, :password_confirmation
+
+  filter :name
+  filter :email
+  filter :introduce
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
 end
