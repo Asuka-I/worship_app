198bd78037d41971209f14036cc030897c24a0ac
Asuka-I <1400cm@gmail.com>
2022-01-23 18:21:42 +0900

    db:seed を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,21 @@ end
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
 
 # 以下，開発用の初期データ
-user1 = User.create!(email: "satou@example.com", password: "password")
-user2 = User.create!(email: "suzuki@example.com", password: "password")
-user3 = User.create!(email: "tanaka@example.com", password: "password")
+user1 = User.create!(name: "さと", email: "satou@example.com", password: "password", introduce: "日本中の寺社仏閣を巡りたい！")
+user2 = User.create!(name: "suzuki", email: "suzuki@example.com", password: "password", introduce: "神社巡りが趣味です。")
+user3 = User.create!(name: "タナ吉", email: "tanaka@example.com", password: "password", introduce: "最近御朱印集めにハマりました。")
 
 puts "ユーザデータの投入に成功しました！"
 
-worship1 = user1.worships.create!(category: "寺", prefecture_id: "14", place: "寒川神社", content: "いつも年末年始にお世話になっています", date: "2021/09/17")
-worship2 = user2.worships.create!(category: "神社", prefecture_id: "20", place: "諏訪大社", content: "見応えがあり、社ごとに雰囲気が違ってゆっくり楽しめた。", date: "2021/09/13")
-worship3 = user3.worships.create!(category: "寺", prefecture_id: "14", place: "常泉寺", content: "彼岸花が咲き誇り、とても美しいお寺。河童さんが沢山いた！", date: "2021/09/08")
+worship1 = user1.worships.create!(category: "寺", prefecture_id: "14", address: "高座郡寒川町宮山3916", place: "寒川神社", content: "いつも年末年始にお世話になっています", date: "2021/09/17")
+worship2 = user2.worships.create!(category: "神社", prefecture_id: "20", address: "諏訪市中洲宮山1", place: "諏訪大社", content: "見応えがあり、社ごとに雰囲気が違ってゆっくり楽しめた。", date: "2021/09/13")
+worship3 = user3.worships.create!(category: "寺", prefecture_id: "14", address: "大和市福田2176", place: "常泉寺", content: "彼岸花が咲き誇り、とても美しいお寺。河童さんが沢山いた！", date: "2021/09/08")
 
 puts "worshipsデータの投入に成功しました！"
 
-seal1 = user1.seals.create!(category: "寺", prefecture_id: "14", place: "寒川神社", date: "2021/09/17")
-seal2 = user2.seals.create!(category: "神社", prefecture_id: "20", place: "諏訪大社", date: "2021/09/13")
-seal3 = user3.seals.create!(category: "寺", prefecture_id: "14", place: "常泉寺", date: "2021/09/08")
+seal1 = user1.seals.create!(category: "寺", prefecture_id: "14", address: "高座郡寒川町宮山3916", place: "寒川神社", date: "2021/09/17")
+seal2 = user2.seals.create!(category: "神社", prefecture_id: "20", address: "諏訪市中洲宮山1", place: "諏訪大社", date: "2021/09/13")
+seal3 = user3.seals.create!(category: "寺", prefecture_id: "14", address: "大和市福田2176", place: "常泉寺", date: "2021/09/08")
 
 puts "sealsデータの投入に成功しました！"
 
