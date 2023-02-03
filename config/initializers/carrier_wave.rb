70dc7983fbf00ebbd34d0f40fd8e7f54a442f3e3
Asuka-I <1400cm@gmail.com>
2023-02-03 20:54:58 +0900

    carrier_wave.rbの設定を変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, :region)
-    }
-    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
-    config.asset_host = "https://dp1br4urcrphw.cloudfront.net"
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
